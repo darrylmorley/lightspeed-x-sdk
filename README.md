64aacc643643f3687383012d652bc7f5f690ef3f
Darryl Morley <darrylmorley.uk@gmail.com>
2023-02-14 16:36:02 +0000

    first commit

--- README.md
+++ README.md
@@ -1,19 +1,11 @@
-# Another Unofficial Lightspeed Retail V3 API SDK for Node.
+# An Unofficial SDK for Lightspeed Retail X (formerly Vend)
 
 ## Work In Progress
 
-First, import LightSpeed Retail SDK:- import LightspeedRetailSDK from "./index.js";
-
-Then intialise a new instance of LightspeedRetailSDK:-
-
 ```
-const api = new LightspeedRetailSDK({
-  accountID: "Your Account No.",
-  clientID: "Your client ID.",
-  clientSecret: "Your client secret.",
-  refreshToken: "Your refresh token.",
-});
+import Vend from "./index.js";
 
-const item = await api.getItem(7947, '["Category", "Images"]');
-console.log(item);
+const api = new Vend("domain", access_token);
+
+const products = await api.get("/products");
 ```
--- index.js
+++ index.js
@@ -1,340 +1,108 @@
 import axios from "axios";
 
-// Cost per operation
-const getRequestUnits = (operation) => {
-  switch (operation) {
-    case "GET":
-      return 1;
-    case "POST":
-      return 10;
-    case "PUT":
-      return 10;
-    default:
-      return 10;
-  }
-};
-
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-class LightspeedRetailSDK {
-  constructor(opts) {
-    const { clientID, clientSecret, refreshToken, accountID } = opts;
-
-    this.clientID = clientID;
-    this.clientSecret = clientSecret;
-    this.refreshToken = refreshToken;
-    this.accountID = accountID;
-    this.baseUrl = "https://api.lightspeedapp.com/API/V3/Account";
-    this.maxRetries = 3;
-    this.lastResponse = null;
+class Vend {
+  constructor(domain, accessToken) {
+    this.domain = domain;
+    this.accessToken = accessToken;
+    this.baseUrl = `https://${domain}.vendhq.com/api/2.0`;
+    this.rateLimitRemaining = null;
+    this.rateLimitReset = null;
+    this.rateLimitInterval = null;
   }
 
-  handleError(msg, err) {
-    console.error(`${msg} - ${err}`);
-    throw err;
+  // async get(endpoint, params = {}) {
+  //   const url = this.buildUrl(endpoint, params);
+  //   const response = await this.fetchWithRateLimit(url, "GET");
+  //   const data = response.data;
+  //   const result = {
+  //     data: data.data,
+  //     version: response.headers["x-pagination-version"],
+  //   };
+  //   return result;
+  // }
+
+  async get(endpoint, params = {}) {
+    let result = { data: [] };
+    let version = null;
+    let responseData;
+
+    do {
+      // Set the `after` parameter for pagination, if available
+      if (version !== null) {
+        params.after = version;
+      }
+
+      // Build the request URL with the endpoint and query parameters
+      const url = this.buildUrl(endpoint, params);
+
+      // Make the request with the rate limiting logic
+      const response = await this.fetchWithRateLimit(url, "GET");
+
+      // Extract the data and `version` attribute from the response
+      responseData = response.data.data;
+      version = response.data.version.max;
+
+      // Add the current page of data to the results
+      result.data = result.data.concat(responseData);
+    } while (responseData.length > 0); // Continue looping until an empty collection is returned
+
+    return result;
   }
 
-  setLastResponse = (response) => (this.lastResponse = response);
+  async post(endpoint, data) {
+    const url = this.buildUrl(endpoint);
+    const response = await this.fetchWithRateLimit(url, "POST", data);
+    return response.data;
+  }
 
-  handleRateLimit = async (options) => {
-    if (!this.lastResponse) return null;
-
-    const { method } = options;
-
-    const requestUnits = getRequestUnits(method);
-    const rateHeader = this.lastResponse.headers["x-ls-api-bucket-level"];
-
-    if (!rateHeader) return null;
-
-    const [used, available] = rateHeader.split("/");
-    const availableUnits = available - used;
-    if (requestUnits <= availableUnits) return 0;
-
-    const dripRate = this.lastResponse.headers["x-ls-api-drip-rate"];
-    const unitWait = requestUnits - availableUnits;
-    const delay = Math.ceil((unitWait / dripRate) * 1000);
-    await sleep(delay);
-
-    return unitWait;
-  };
-
-  getToken = async () => {
-    const body = {
-      grant_type: "refresh_token",
-      client_id: this.clientID,
-      client_secret: this.clientSecret,
-      refresh_token: this.refreshToken,
-    };
-
+  async fetchWithRateLimit(url, method, data) {
+    // Wait until the rate limit interval is over, if necessary
+    await this.waitUntilRateLimitReset();
+    // Make the request and update the rate limit information
     const response = await axios({
-      url: "https://cloud.lightspeedapp.com/oauth/access_token.php",
-      method: "post",
+      method,
+      url,
       headers: {
+        Authorization: `Bearer ${this.accessToken}`,
         "Content-Type": "application/json",
       },
-      data: JSON.stringify(body),
-    }).catch((error) => console.error(error.response.data));
-
-    const tokenData = await response.data;
-    const token = tokenData.access_token;
-
-    return token;
-  };
-
-  getResource = async (options, retries = 0) => {
-    this.handleRateLimit(options);
-
-    const token = await this.getToken();
-
-    if (!token) throw new Error("Error Fetching Token");
-
-    options.headers = {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    };
-
-    try {
-      const res = await axios(options);
-      this.lastResponse = res;
-      return {
-        data: res.data,
-        next: res.next,
-        previous: res.previous,
-      };
-    } catch (err) {
-      if (retries < this.maxRetries) {
-        console.log(`Error: ${err}, retrying in 2 seconds...`);
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        return await this.getResource(url, retries + 1);
-      } else {
-        console.error(`Failed Request statusText: `, res.statusText);
-        console.log(`Failed data: `, response.data);
-        throw err;
-      }
-    }
-  };
-
-  async getAllData(options) {
-    let allData = [];
-    while (options.url) {
-      const { data } = await this.getResource(options);
-      let next = data["@attributes"].next;
-      let selectDataArray = Object.keys(data)[1];
-      let selectedData = data[selectDataArray];
-      allData = allData.concat(selectedData);
-      options.url = next;
-    }
-    // console.log(allData);
-    return allData;
+      data: data,
+    });
+    this.updateRateLimit(response);
+    return response;
   }
 
-  async getCustomers(relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Customer.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET CUSTOMERS ERROR", error.response);
+  buildUrl(endpoint, params = {}) {
+    let url = `${this.baseUrl}${endpoint}`;
+    const searchParams = new URLSearchParams(params);
+    if (searchParams.toString()) {
+      url += `?${searchParams.toString()}`;
     }
+    return url;
   }
 
-  async getItems(relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Item.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET ITEMS ERROR", error.response);
+  async waitUntilRateLimitReset() {
+    if (this.rateLimitRemaining === null || this.rateLimitRemaining > 0) {
+      // The rate limit has not been exceeded, so no need to wait
+      return;
     }
+    // Wait for the rate limit interval to expire
+    const now = Math.floor(Date.now() / 1000);
+    const delay = this.rateLimitReset - now;
+    await new Promise((resolve) => setTimeout(resolve, delay * 1000));
   }
 
-  async getItem(itemID, relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Item/${itemID}.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET ITEM ERROR", error.response);
+  updateRateLimit(response) {
+    const remaining = response.headers["x-rate-limit-remaining"];
+    if (remaining !== null) {
+      this.rateLimitRemaining = parseInt(remaining);
     }
-  }
-
-  async getCategories(relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Category.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET CATEGORIES ERROR", error.response);
-    }
-  }
-
-  async getCategory(categoryID, relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Category/${categoryID}.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET CATEGORY ERROR", error.response);
-    }
-  }
-
-  async getManufacturers(relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Category.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET MANUFACTURERS ERROR", error.response);
-    }
-  }
-
-  async getManufacturer(manufacturerID, relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Manufacturer/${manufacturerID}.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET MANUFACTURER ERROR", error.response);
-    }
-  }
-
-  async getOrders(relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Order.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET ORDERS ERROR", error.response);
-    }
-  }
-
-  async getOrder(orderID, relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Order/${orderID}.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET ORDER ERROR", error.response);
-    }
-  }
-
-  async getVendors(relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Vendor.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET VENDORS ERROR", error.response);
-    }
-  }
-
-  async getVendor(vendorID, relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Vendor/${vendorID}.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET VENDOR ERROR", error.response);
-    }
-  }
-
-  async getSales(relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Sale.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET SALES ERROR", error.response);
-    }
-  }
-
-  async getSale(saleID, relations) {
-    const options = {
-      url: `${this.baseUrl}/${this.accountID}/Sale/${saleID}.json`,
-      method: "GET",
-    };
-
-    if (relations) options.url = options.url + `?load_relations=${relations}`;
-
-    try {
-      const response = await this.getAllData(options);
-      return response;
-    } catch (error) {
-      return this.handleError("GET SALE ERROR", error.response);
+    const reset = response.headers["x-rate-limit-reset"];
+    if (reset !== null) {
+      this.rateLimitReset = parseInt(reset);
+      this.rateLimitInterval = this.rateLimitReset - Math.floor(Date.now() / 1000);
     }
   }
 }
 
-export default LightspeedRetailSDK;
+export default Vend;
